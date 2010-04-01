21e3101909a444f5c90ea56069b6e65ceced96fa
James Broad <jb@carbonsilk.com>
2010-04-01 14:29:25 -0700

    Adding some fixes for inserting the script node in the document head. Updating the readme documentation, too.

--- README.markdown
+++ README.markdown
@@ -3,17 +3,31 @@ Cross browser focus utilities
 
 Get access to the getters of of focus management found in the HTML5 spec <http://www.w3.org/TR/html5/editing.html#document-level-focus-apis>
 
-Get started:
+Get started
 -----------
-Place the script node (`<script type="text/javascript" src="detect_focus.js"></script>`) inside the head node.
+Place the script node (`<script type="text/javascript" src="detect_focus.js"></script>`) inside the `<head>` node or immediately after the opening body tag. We do this as we want to start capturing focus events as soon as possible.
 
-Using:
+Using
 ------
 In your Javascript you can feel confident using `document.activeElement` and `element.hasFocus()`.
 
-Example:
+Examples
 --------
     var fooEl = document.getElementById('foo');
     if(fooEl.hasFocus()){
         alert('Yay, foo has focus!');
     }
+    
+    document.addEventListener("focus", function(e){
+        if(window.console && window.console.log){
+            console.log(document.activeElement);
+        }
+    }, true);
+
+Known Issues
+------------
+We use the focus event listener to log the active element in the example. This seems to be slightly buggy in Webkit browsers using a mouse and clicking. Try tabbing through to see the item update or just look in a browser that supports `console.log`.
+    
+Credits
+-------
+Many thanks to Dirk Ginader (<http://blog.ginader.de>) for all his help in this.
--- detect_focus.js
+++ detect_focus.js
@@ -3,26 +3,42 @@
     @link http://www.w3.org/TR/html5/editing.html#document-level-focus-apis
     @author James Broad
 */
-
 (function(el) {
-    // Handle !IE browsers that do not support the document.activeElement property
-    if(!document.activeElement){
-        if (document.addEventListener){ 
-            document.addEventListener("focus", function(e){
-		        if (e && e.target){
-		            e.target == document ? null : e.target;
-		            document.activeElement = e.target;
-		        }
-		    }, true);
+    var handleFocus = function(){
+        // Handle !IE browsers that do not support the document.activeElement property
+        document.addEventListener("focus", function(e){
+            if (e && e.target){
+                e.target == document ? null : e.target;
+                document.activeElement = e.target;
+                if(window.console && window.console.log){
+                    console.log('document.activeElement', document.activeElement);
+                }
+            }
+        }, true);
+    };
+        
+    // Covering instance where script node is added in the head tag. Brute force baby.
+    setTimeout(function(){
+        while(!document.activeElement){
+            try{
+                document.activeElement = document;
+                if(window.console && window.console.log){
+                    console.log('document.activeElement is not in this browser, we can run our code');
+                }
+                handleFocus();
+                break;
+            } catch(e) {
+                if(window.console && window.console.log){
+                    console.log(e);
+                }
+                break;
+            }    
         }
-    }
+    }, 10);
 })(document);
+
 if(!document.hasFocus || !HTMLElement.hasFocus){
-	HTMLDocument.prototype.hasFocus = HTMLElement.prototype.hasFocus = function(){
-		if(this == document.activeElement){
-			return true;
-		} else {
-			return false;
-		}
-	}
+    HTMLDocument.prototype.hasFocus = HTMLElement.prototype.hasFocus = function(){
+        return (this == document.activeElement);
+    }
 }
